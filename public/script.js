cf14704ed0174cad2daeaf9fa27b165f3ba4e88f
eyob42 <eyobadigeh07@gmail.com>
2026-03-09 22:24:00 +0300

    Add video streaming functionality and UUID integration
    - Updated package.json and package-lock.json to include 'uuid' dependency.
    - Implemented video streaming in public/script.js.
    - Modified server.js to redirect to a unique room ID using UUID.
    - Updated room.ejs to display video grid.

---
+++ public/script.js
@@ -0,0 +1,20 @@
+const videoGrid = document.getElementById('video-grid')  // video
+const myVideo = document.createElement('video')   // video-tag
+let myVideoStream;
+myVideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+}).then((stream)=>{
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+});
+
+function addVideoStream(video, stream){
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const app = express();
 
 
 const server = require("http").Server(app);
-app.set('view engine', 'ejs')
+const { v4: uuidv4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('room')
+    res.redirect(`/${uuidv4()}`);
+})
+app.get('/:room', (req, res) => {
+    res.render('room', {roomId: req.params.room});
 })
 
 
